e37c5bf8d82bae0baf98ac9a9d12a508c2ba22cb
gaganbiswas <gaganbiswas.me1@gmail.com>
2021-08-04 20:05:22 +0530

    Adding key to StarIcon

--- src/components/Product.js
+++ src/components/Product.js
@@ -46,7 +46,7 @@ function Product({ id, title, price, description, category, image }) {
         {Array(rating)
           .fill()
           .map((_, i) => (
-            <StarIcon className="h-5 text-yellow-500" />
+            <StarIcon className="h-5 text-yellow-500" key={i} />
           ))}
       </div>
 
