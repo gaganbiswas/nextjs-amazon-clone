57cf9f43f5ec7af7bbeecc9892a0e7c4fa939486
gaganbiswas <gaganbiswas.me1@gmail.com>
2021-08-04 20:00:52 +0530

    Initial Commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQdBVoZ2jcgeV83nBULnrR9lUSOY3fGR0",
+  authDomain: "clone-390bd.firebaseapp.com",
+  projectId: "clone-390bd",
+  storageBucket: "clone-390bd.appspot.com",
+  messagingSenderId: "337909098149",
+  appId: "1:337909098149:web:fc930b53ea599022d78f4c",
+};
